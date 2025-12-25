60f1833a4da6fa2a635186a337719ec14038fbc8
Mehidi Hasan <hridoy21.21@gmail.com>
2025-12-26 00:39:30 +0600

    Navabr has updated with full  designe

--- website/tests.py
+++ website/tests.py
@@ -7,12 +7,12 @@ python manage.py migrate
 python manage.py runserver
 
 git add .
-git commit -m "Navabr has updated with full responsive designe"
+git commit -m "Navabr has updated with full  designe"
 git push -u origin main
 
 
 git add .
-git commit -m "Navabr has updated with full responsive designe"
+git commit -m "Navabr has updated with full  designe"
 git push heroku main
 
 python manage.py runserver
