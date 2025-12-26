edcef019d6d324fe869671a21223dc0423f4bfac
Mehidi Hasan <hridoy21.21@gmail.com>
2025-12-26 09:43:17 +0600

    Navabr has updated

--- website/tests.py
+++ website/tests.py
@@ -7,12 +7,12 @@ python manage.py migrate
 python manage.py runserver
 
 git add .
-git commit -m "Navabr has updated with full  designe"
+git commit -m "Navabr has updated "
 git push -u origin main
 
 
 git add .
-git commit -m "Navabr has updated with full  designe"
+git commit -m "Navabr has updated "
 git push heroku main
 
 python manage.py runserver
